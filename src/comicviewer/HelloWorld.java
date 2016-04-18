4874f67694ba028bb2a87e723941cdda8d634f4d
Monica E McGrath <Monica E McGrath@HungryAlpaca>
2016-04-18 18:00:20 -0500

    Put page changing into a method. Added some menu functionality.

--- src/comicviewer/HelloWorld.java
+++ src/comicviewer/HelloWorld.java
@@ -18,6 +18,11 @@ public class HelloWorld extends Application{
 	final int SCENE_HEIGHT = 700;
 	final int SCENE_WIDTH = 1500;
 	int numPages = 0;
+	PDFNode node = new PDFNode();
+	Button left = new Button("Previous Page");
+	Button right = new Button("Next Page");
+	MenuItem next = new MenuItem("Next Page");
+	final MenuItem previous = new MenuItem("Previous Page");
 	@Override
 	public void start(Stage stage) throws IOException{
 //		stage.setResizable(false);
@@ -28,36 +33,15 @@ public class HelloWorld extends Application{
 		hbox.setAlignment(Pos.CENTER);
 		Scene scene = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
 		
-		PDFNode node = new PDFNode();
+		
 		TextField pageNumber = new TextField();
-		Button left = new Button("Previous Page");
-		Button right = new Button("Next Page");
 		
 		left.setOnAction(new EventHandler<ActionEvent>(){
 
 			@Override
 			public void handle(ActionEvent arg0) {
 				int page = node.getPageNumber();
-				System.out.println("current page: " + page);
-				if (page - 1 == 0)
-				{
-					left.setDisable(true);
-				}
-				try{
-					node.setPageNumber(page - 1);
-					System.out.println("we just set this!!!!" + node.getPageNumber());
-				}
-				catch (PDFException e)
-				{
-					System.out.println("beginning of book was reached. Page Number: " + node.getPageNumber());
-				}
-				
-			//	if (currentPage - 1 == 0)
-			//	{
-			//		left.setDisable(true);
-			//	}
-				
-				right.setDisable(false);
+				goToPage(page - 1);
 			}
 			
 		});
@@ -66,29 +50,8 @@ public class HelloWorld extends Application{
 
 			@Override
 			public void handle(ActionEvent event) {
-				//currentPage += 1;
-				//System.out.println("currentPage: " + currentPage);
-				//node.setPageNumber(currentPage);
 				int page = node.getPageNumber();
-				try{
-					node.setPageNumber(page + 1);
-					System.out.println("we are now on this page moving forwards: " + node.getPageNumber());
-				}
-				catch (PDFException e)
-				{
-					System.out.println("end of book was reached");
-				}
-				
-				if (page + 1 == (numPages - 1))
-				{
-					right.setDisable(true);
-				}
-				//if (currentPage + 1 == (numPages - 1))
-				//{
-			//		right.setDisable(true);
-			//	}
-				
-				left.setDisable(false);
+				goToPage(page + 1);
 			}
 			
 		});
@@ -97,29 +60,26 @@ public class HelloWorld extends Application{
 		final Menu file = new Menu( "File" );
 	    final Menu comic = new Menu("Comic");
 	    
-		MenuItem next = new MenuItem("Next Page");
+		
 		next.setVisible(true);
 		next.setOnAction(e -> {});
-		final MenuItem previous = new MenuItem("Previous Page");
+		
 		previous.setOnAction(e -> {});
+		
+		final MenuItem bookmark = new MenuItem("Bookmark");
+		bookmark.setOnAction(e -> {});
 		
 		final MenuItem open = new MenuItem("Open Comic");
 		open.setOnAction(e -> {
 			if (!root.getChildren().contains(node))
 			{
-				//System.out.println("i am adding a node");
 				ScrollPane nodeBox = new ScrollPane();
 				nodeBox.setFitToWidth(true);
 				nodeBox.setFitToHeight(true);
 				nodeBox.setContent(node);
-//				Button button = new Button("wat");
-//				button.setStyle("-fx-background-color: #0FF;");
-//				nodeBox.setContent(button);
 				nodeBox.setStyle("-fx-background-color: purple;");
 				node.setStyle("-fx-background-color: cyan;");
 				root.setCenter(nodeBox);
-				//System.out.println( nodeBox.getBoundsInParent() );
-//				StackPane.setAlignment(node, Pos.CENTER);
 			}
 			
 			try{
@@ -127,10 +87,11 @@ public class HelloWorld extends Application{
 				node.openFile(fileToOpen);
 				numPages = node.getNumPages();
 				System.out.println("page upon open: " + node.getPageNumber());
-				//currentPage = node.getPageNumber();
-				//System.out.println("currentPage on open: " + currentPage);
-//				System.out.println("File opened");
-//				System.out.println(pdf);
+				right.setDisable(false);
+				next.setDisable(false);
+				pageNumber.setDisable(false);
+				bookmark.setDisable(false);
+	
 			} catch(IOException exception){
 				exception.printStackTrace();
 				System.err.println("Couldn't open the pdf");
@@ -139,14 +100,22 @@ public class HelloWorld extends Application{
 			
 		});
 		
-		final MenuItem bookmark = new MenuItem("Bookmark");
-		bookmark.setOnAction(e -> {});
+		
 		
 		final MenuItem darkSide = new MenuItem("Dark Mode");
 		final MenuItem lightSide = new MenuItem("Light Mode");
 		
 		final MenuItem close = new MenuItem( "Close" );
 		close.setOnAction( (e) -> { stage.close(); } );
+		
+		previous.setOnAction((e) -> {
+			int page = node.getPageNumber();
+			goToPage(page - 1);
+		});
+		next.setOnAction((e) -> {
+			int page = node.getPageNumber();
+			goToPage(page + 1);
+		});
 		
 		file.getItems().add(open);
 		file.getItems().add(bookmark);
@@ -156,18 +125,22 @@ public class HelloWorld extends Application{
 		
 		comic.getItems().add(next);
 		comic.getItems().add(previous);
-		
+	
 		bar.getMenus().add( file );
 		bar.getMenus().add(comic);
 		
 		left.setDisable(true);
+		right.setDisable(true);
+		pageNumber.setDisable(true);
+		bookmark.setDisable(true);
+		previous.setDisable(true);
+		next.setDisable(true);
+		
 		hbox.getChildren().addAll(left, pageNumber, right);
 		hbox.setSpacing(10);
 		hbox.setStyle("-fx-background-color: #780000;");
 		root.setTop(bar);
 		root.setBottom(hbox);
-//		StackPane.setAlignment(bar, Pos.TOP_CENTER);
-//		StackPane.setAlignment(hbox, Pos.BOTTOM_CENTER);
 		root.setStyle("-fx-background-color: #336699;");
 		stage.setScene(scene);
 		stage.show();
@@ -182,6 +155,41 @@ public class HelloWorld extends Application{
 		FileChooser out = new FileChooser();
 		out.getExtensionFilters().add(new ExtensionFilter("PDF Files", "*.pdf"));
 		return out;
+	}
+	
+	public void goToPage(int pageNumber){
+		
+		try{
+			node.setPageNumber(pageNumber);
+			System.out.println("we just set this!!!!" + node.getPageNumber());
+		}
+		catch (PDFException e)
+		{
+			System.out.println("a book end was reached. Page Number: " + node.getPageNumber());
+		}
+		
+		if (pageNumber - 1 == 0)
+		{
+			previous.setDisable(true);
+			left.setDisable(true);
+		}
+		
+		else
+		{
+			previous.setDisable(false);
+			left.setDisable(false);
+		}
+		
+		if (pageNumber + 1 == node.getNumPages() - 1)
+		{
+			next.setDisable(true);
+			right.setDisable(true);
+		}
+		else
+		{
+			next.setDisable(false);
+			right.setDisable(false);
+		}
 	}
 	
 }
